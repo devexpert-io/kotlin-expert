b1eecc3dc7d5a15874c3589ad4a091860d2adade
antoniolg <antonio@devexperto.com>
2022-06-21 15:03:02 +0200

    Exercise: Practice with variables and string templates

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,8 +1,10 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -12,13 +14,17 @@ import androidx.compose.ui.window.application
 @Composable
 @Preview
 fun App() {
-    val text = remember { mutableStateOf("Goodbye, World!") }
+    val text = remember { mutableStateOf("") }
 
     MaterialTheme {
-        Button(onClick = {
-            text.value = "Hello, Desktop!"
-        }) {
-            Text(text.value)
+        Column {
+            TextField(value = "", onValueChange = { newText -> TODO() })
+            Text(text = "")
+            Button(onClick = {
+                text.value = TODO()
+            }) {
+                Text("Clean")
+            }
         }
     }
 }
