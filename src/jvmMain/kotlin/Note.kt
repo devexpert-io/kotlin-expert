be6cd280994e2adf983f2d91fe7b3aa19b87a8eb
antoniolg <antonio@devexperto.com>
2022-06-24 11:55:56 +0200

    Exercise: Build a set of notes with ranges and flow controls

--- src/jvmMain/kotlin/Note.kt
+++ src/jvmMain/kotlin/Note.kt
@@ -4,15 +4,10 @@ data class Note(val title: String, val description: String, val type: Type) {
     enum class Type { TEXT, AUDIO }
 }
 
-val list = listOf(
-    Note("Title 1", "Description 1", Type.TEXT),
-    Note("Title 2", "Description 2", Type.TEXT),
-    Note("Title 3", "Description 3", Type.TEXT),
-    Note("Title 4", "Description 4", Type.AUDIO),
-    Note("Title 5", "Description 5", Type.TEXT),
-    Note("Title 6", "Description 6", Type.TEXT),
-    Note("Title 7", "Description 7", Type.TEXT),
-    Note("Title 8", "Description 8", Type.AUDIO),
-    Note("Title 9", "Description 9", Type.TEXT),
-    Note("Title 10", "Description 10", Type.TEXT)
-)
+fun getNotes(): List<Note> = (0..10).map {
+    Note(
+        "Title $it",
+        "Description $it",
+        if (it % 3 == 0) Type.AUDIO else Type.TEXT
+    )
+}
