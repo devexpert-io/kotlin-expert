16c97a7be8632075acfe56efae36e1b6a734628e
antoniolg <antonio@devexperto.com>
2022-06-24 10:50:53 +0200

    Build list of Notes

--- src/jvmMain/kotlin/Note.kt
+++ src/jvmMain/kotlin/Note.kt
@@ -1,1 +1,14 @@
 data class Note(val title: String, val description: String)
+
+val list = listOf(
+    Note("Title 1", "Description 1"),
+    Note("Title 2", "Description 2"),
+    Note("Title 3", "Description 3"),
+    Note("Title 4", "Description 4"),
+    Note("Title 5", "Description 5"),
+    Note("Title 6", "Description 6"),
+    Note("Title 7", "Description 7"),
+    Note("Title 8", "Description 8"),
+    Note("Title 9", "Description 9"),
+    Note("Title 10", "Description 10")
+)
