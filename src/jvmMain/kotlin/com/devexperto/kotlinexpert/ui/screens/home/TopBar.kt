ae12fb4f8503e62b4c396a5ce966086351038a69
antoniolg <antonio@devexperto.com>
2022-08-25 11:05:30 +0200

    Revert "Exercise: Build filter menu using an infix local function"
    This reverts commit 43c24f05b65aa11fdb04f9cae701238ab3cc6251.

--- src/jvmMain/kotlin/com/devexperto/kotlinexpert/ui/screens/home/TopBar.kt
+++ src/jvmMain/kotlin/com/devexperto/kotlinexpert/ui/screens/home/TopBar.kt
@@ -17,27 +17,25 @@ fun TopBar(onFilterClick: (Filter) -> Unit) {
 
 @Composable
 private fun FiltersAction(onFilterClick: (Filter) -> Unit) {
-
     var expanded by remember { mutableStateOf(false) }
-
-    @Composable
-    infix fun Filter.ToMenuItem(text: String) {
-        DropdownMenuItem(onClick = {
-            onFilterClick(this)
-            expanded = false
-        }) {
-            Text(text)
-        }
-    }
 
     IconButton(onClick = { expanded = true }) {
 
         Icon(imageVector = Icons.Default.FilterList, contentDescription = "Filter")
 
         DropdownMenu(expanded, onDismissRequest = { expanded = false }) {
-            Filter.All ToMenuItem "All"
-            Filter.ByType(Note.Type.TEXT) ToMenuItem "Text"
-            Filter.ByType(Note.Type.AUDIO) ToMenuItem "Audio"
+            listOf(
+                Filter.All to "All",
+                Filter.ByType(Note.Type.TEXT) to "Text",
+                Filter.ByType(Note.Type.AUDIO) to "Audio"
+            ).forEach { (filter, text) ->
+                DropdownMenuItem(onClick = {
+                    onFilterClick(filter)
+                    expanded = false
+                }) {
+                    Text(text)
+                }
+            }
         }
     }
 }
