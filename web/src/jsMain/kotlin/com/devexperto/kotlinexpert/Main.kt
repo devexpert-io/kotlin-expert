91c32363bba1388156b6e31e81f2ca8d972c97d5
antoniolg <antonio@devexperto.com>
2023-03-22 16:50:46 +0100

    Web: Kotlin/JS simple sample

--- web/src/jsMain/kotlin/com/devexperto/kotlinexpert/Main.kt
+++ web/src/jsMain/kotlin/com/devexperto/kotlinexpert/Main.kt
@@ -2,13 +2,35 @@ package com.devexperto.kotlinexpert
 
 import com.devexperto.kotlinexpert.ui.App
 import com.devexperto.kotlinexpert.ui.theme.AppStyleSheet
+import kotlinx.browser.document
+import kotlinx.browser.window
 import org.jetbrains.compose.web.css.Style
 import org.jetbrains.compose.web.renderComposable
 
 fun main() {
+    composeSample()
+    kotlinJsSample()
+}
+
+private fun composeSample() {
+    document.getElementById("root") ?: return
     renderComposable(rootElementId = "root") {
         Style(AppStyleSheet)
         App()
+    }
+}
+
+private fun kotlinJsSample() {
+    window.onload = {
+        val message = document.getElementById("message")
+        if (message != null) {
+            message.textContent = "Hello, Kotlin/JS!"
+
+            val button = document.getElementById("button")!!
+            button.addEventListener("click", {
+                window.alert("You clicked the button!")
+            })
+        }
     }
 }
 
